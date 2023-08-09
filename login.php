630e62e1596f417e3070e409c975ee9ccd3229b9
sadani <129391563+sadanisamarasinghe@users.noreply.github.com>
2023-08-09 16:55:30 +0530

    Update login.php

--- login.php
+++ login.php
@@ -39,31 +39,7 @@
     <hr>
     <p align="center">Not registered? <a href="register.php">Create an account</a></p>
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-		
-
-         <!--bootstrap js link-->
+ <!--bootstrap js link-->
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" 
     integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
     
